298b0e0985adf1a080bcc1b82fc7a6b54782cd94
DemonDis <50297797+DemonDis@users.noreply.github.com>
2023-03-30 19:46:58 +0700

    🚧 test work

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   // pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
   // pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
   reactStrictMode: true,
+  assetPrefix: "./",
+  output: 'export',
   async redirects() {
     return [
       {
--- src/pagesLayer/login/model/index.js
+++ src/pagesLayer/login/model/index.js
@@ -1,3 +1,3 @@
 export { fetchFx, fetchPostsFx, fetchTodosFx} from './controller'
-export { model } from './model.js' 
+// export { model } from './model.js' 
 export { $todos, $todos2 } from './state'
